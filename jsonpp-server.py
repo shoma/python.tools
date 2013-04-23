ed5d62510cd65144bc99885ad5d63c15e2304aed
shoma <shoma@catbot.net>
2013-04-23 22:13:05 +0900

    Remove invalid doc.

--- jsonpp-server.py
+++ jsonpp-server.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-r"""Command-line tool to validate and pretty-print JSON
-
-Usage::
-
-    $ echo '{"json":"obj"}' | python -m json.tool
-    {
-        "json": "obj"
-    }
-    $ echo '{ 1.2:3.4}' | python -m json.tool
-    Expecting property name enclosed in double quotes: line 1 column 2 (char 2)
-
-"""
 import argparse
 import cgi
 import json
